dfb5e60fe21f567872c96599fc8ad7dbda445066
Greg Bergé <greg@smooth-code.com>
2020-09-05 19:00:07 +0200

    chore: update packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   plugins: [
     {
-      resolve: 'smooth-doc',
+      resolve: "smooth-doc",
       options: {
-        name: 'Smooth DOC Example',
-        description: 'Ready to use documentation theme for Gatsby.',
-        siteUrl: 'https://smooth-doc.com',
+        name: "Smooth DOC Starter",
+        description: "Use your own description...",
+        siteUrl: "https://example.com",
       },
     },
   ],
-}
+};
